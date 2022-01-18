1ef6e71f8268245283def4a106e3b844e072590c
loicfmx <loic.fremaux1@gmail.com>
2022-01-18 17:46:52 +0100

    WIP (.obj files support)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 import {STLLoader} from "three/examples/jsm/loaders/STLLoader";
+import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader.js";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 require('./bootstrap');
@@ -34,6 +35,7 @@ window.create3DBox = function create3DBox(filePath, htmlElementName) {
     }
 
     let camera = new THREE.PerspectiveCamera(70, (htmlElement.clientWidth / htmlElement.clientHeight), 1, 1000)
+    camera.position.set(0, 0, 10);
 
     let renderer = new THREE.WebGLRenderer({
         antialias: true,
@@ -56,6 +58,8 @@ window.create3DBox = function create3DBox(filePath, htmlElementName) {
     controls.enableZoom = true;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 1;
+    controls.target.set(0, 0, 0);
+    controls.update();
 
     let scene = new THREE.Scene();
     scene.add(new THREE.HemisphereLight(0xffffff, 1.5));
@@ -64,39 +68,63 @@ window.create3DBox = function create3DBox(filePath, htmlElementName) {
 
     window.tasks[taskId] = htmlElementName;
 
-    (new STLLoader()).load(filePath, function (geometry) {
+    let type;
+    let func = function (geometry) {
         let material = new THREE.MeshPhongMaterial({
             color: 0x00d1e2,
             specular: 100,
             shininess: 100
         });
 
-        let mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+        if (geometry.type === 'Group') {
+            geometry.traverse(child => {
+                if (child instanceof THREE.Mesh) {
+                    child.material = material;
+                    child.geometry.computeVertexNormals();
+                }
+            });
+        }
 
-        let middle = new THREE.Vector3();
-        geometry.computeBoundingBox();
-        geometry.boundingBox.getCenter(middle);
-        mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(
-            -middle.x, -middle.y, -middle.z));
+        if (type === 'stl') {
+            let mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
 
-        let largestDimension = Math.max(geometry.boundingBox.max.x,
-            geometry.boundingBox.max.y,
-            geometry.boundingBox.max.z)
-        camera.position.z = largestDimension * 2.5; // corresponds to default zoom
+            let middle = new THREE.Vector3();
+            geometry.computeBoundingBox();
+            geometry.boundingBox.getCenter(middle);
+            mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(
+                -middle.x, -middle.y, -middle.z));
+
+            let largestDimension = Math.max(geometry.boundingBox.max.x,
+                geometry.boundingBox.max.y,
+                geometry.boundingBox.max.z)
+            camera.position.z = largestDimension * 2.5; // corresponds to default zoom
+        } else {
+            scene.add(geometry)
+        }
 
         let animate = function () {
             if (window.tasks[taskId] === null) {
                 console.log('task ' + taskId + ' stopped')
                 return;
             }
+
             requestAnimationFrame(animate);
             controls.update();
             renderer.render(scene, camera);
         };
 
         animate();
-    });
+    };
+
+    const manager = new THREE.LoadingManager();
+    if (filePath.endsWith('.stl')) {
+        type = 'stl';
+        (new STLLoader(manager)).load(filePath, func);
+    } else {
+        type = 'obj';
+        (new OBJLoader(manager)).load(filePath, func);
+    }
 
     return taskId;
 }
